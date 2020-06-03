ac6e632594bb52fa64a65f664b9c054201df1bfa
JACK #EZ <jack@mayers-air.lan>
2020-06-03 15:13:41 +0800

    Initial Commit

---
+++ Chess2/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  Chess2
+//
+//  Created by JACK #EZ on 3/6/2020.
+//  Copyright © 2020 JACK #EZ. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
